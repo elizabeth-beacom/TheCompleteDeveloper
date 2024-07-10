05d9f9508a6596c4803753a26c3f4474407b72c1
Elizabeth Beacom <20716418+elizabeth-beacom@users.noreply.github.com>
2024-07-09 19:04:39 -0700

    Chapter 7 completed. Connected GraphQL API to the Database

--- sample-next/refactored-app/graphql/resolvers.ts
+++ sample-next/refactored-app/graphql/resolvers.ts
@@ -1,5 +1,5 @@
 import { PathParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
-import { db } from "./data";
+import { findByZip, updateByZip } from "@/mongoose/weather/services";
 
 declare interface WeatherInterface {
     zip: string;
@@ -12,12 +12,15 @@ declare interface WeatherInterface {
 export const resolvers = {
     Query: {
         weather: async (_: any, param: WeatherInterface) => {
-            return [db.find((item) => item.zip === param.zip)];
+            let data = await findByZip(param.zip);
+            return [data];
         }
     },
     Mutation: {
         weather: async (_: any, param: {data: WeatherInterface}) => {
-            return [db.find((item) => item.zip === param.data.zip)];
+            await updateByZip(param.data.zip, param.data);
+            let data = await findByZip(param.data.zip);
+            return [data];
         }
     }
 };
--- sample-next/refactored-app/pages/api/weather/[zipcode].ts
+++ sample-next/refactored-app/pages/api/weather/[zipcode].ts
@@ -1,4 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { findByZip } from "@/mongoose/weather/services";
+import dbConnect from "@/middleware/db-connect";
+dbConnect();
 
 type WeatherDetailType = {
     zipcode: string;
@@ -8,9 +11,6 @@ type WeatherDetailType = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse):
     Promise<NextApiResponse<WeatherDetailType> | void> {
-        return res.status(200).json({
-            zipcode: req.query.zipcode,
-            weather: "sunny",
-            temp: 35
-        });
+        let data = await findByZip(req.query.zipcode as string);
+        return res.status(200).json(data);
     }
